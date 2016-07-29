17c9af4cec034ce97715f2d7473a0660f8667b9e
刘明 <LM@liuming.local>
2016-07-29 16:28:55 +0800

    初始化项目

---
+++ LMFoldHeadView/LMFoldHeadView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LMFoldHeadView
+//
+//  Created by 刘明 on 16/7/29.
+//  Copyright © 2016年 刘明. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ LMFoldHeadView/LMFoldHeadView/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  LMFoldHeadView
+//
+//  Created by 刘明 on 16/7/29.
+//  Copyright © 2016年 刘明. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
